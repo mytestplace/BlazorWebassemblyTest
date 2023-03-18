d89d6b1839c928f6cb0e324a5adeaaf2ff5a64a0
ishwar <mr.whybay@gmail.com>
2023-03-18 21:45:14 +0530

    Add project files.

---
+++ BlazorStudentRegistration/Server/DbModel/StudentDBContext.cs
@@ -0,0 +1,16 @@
+﻿using BlazorStudentRegistration.Shared.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BlazorStudentRegistration.Server.DbModel
+{
+    public class StudentDBContext : DbContext
+    {
+        public StudentDBContext(DbContextOptions<StudentDBContext> options)
+        : base(options)
+        {
+        }
+        
+        public DbSet<Students> Students { get; set; }
+    }
+}
---
+++ BlazorStudentRegistration/Shared/Model/Students.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorStudentRegistration.Shared.Model
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name  { get; set; }
+        public string DateOfBirth { get; set; }
+        public string  Class_ { get; set; }
+        public int  MobileNumber { get; set; }
+        public string  Email { get; set; }
+        public bool IsActive { get; set; }
+        
+    }
+}
